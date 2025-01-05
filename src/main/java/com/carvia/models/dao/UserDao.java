0c0fe87a7909ad8710054146a82a34e8f81abff5
jminac00 <jminac00@estudiantes.unileon.es>
2025-01-05 23:04:06 +0100

    Añadido listUsers() y eliminarUser() de la clase UserDao

--- src/main/java/com/carvia/models/dao/UserDao.java
+++ src/main/java/com/carvia/models/dao/UserDao.java
@@ -8,8 +8,10 @@ import java.sql.Connection;
  import java.sql.PreparedStatement;
  import java.sql.ResultSet;
  import java.sql.SQLException;
- 
- import org.apache.logging.log4j.LogManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
  import org.apache.logging.log4j.Logger;
  import org.mindrot.jbcrypt.BCrypt;
  
@@ -76,6 +78,48 @@ import java.sql.Connection;
         }
         return null;
      }
+    
+    public List<UserVo> listarUsuarios() throws Exception {
+        List<UserVo> usuarios = new ArrayList<>();
+        String query = "SELECT * FROM Users";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("idUs");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                usuarios.add(new UserVo(id, username, password));
+                System.out.println("Usuarios listados correctamente.");
+            }
+        } catch (SQLException e) {
+            logger.error("Error listing users");
+            System.out.println("Error listando los usuarios.");
+            e.printStackTrace();
+            throw new Exception("Error listing users", e);
+        }
+        return usuarios;
+    }
+
+    public void eliminarUsuario(UserVo user) throws Exception {
+        String query = "DELETE FROM Users WHERE idUs = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, user.getId());
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                logger.info("User " + user.getUsername() + " deleted successfully");
+                System.out.println("User " + user.getUsername() + " deleted successfully");
+            } else {
+                logger.warn("No user found with id " + user.getId());
+                System.out.println("No user found with id " + user.getId());
+            }
+        } catch (SQLException e) {
+            logger.error("Error deleting user " + user.getUsername());
+            System.out.println("Error deleting user " + user.getUsername());
+            e.printStackTrace();
+            throw new Exception("Error deleting user", e);
+        }
+    }
+    
  }
 
 
